0e4d402e0746e0bd6fe4a10503d693d0f8b52510
Haidercc10 <cantillohaider10@gmail.com>
2022-09-05 16:15:09 -0500

    Ultimo Cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicioPruebaService } from './Servicios/servicio-prueba.service';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ServicioPruebaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
